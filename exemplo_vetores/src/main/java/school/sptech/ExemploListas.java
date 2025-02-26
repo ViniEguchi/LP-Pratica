655c0aa96fb8c15df26294c6e83ba263b10641b5
ViniEguchi <vinicius.eguchi@sptech.school>
2025-02-26 20:06:05 -0300

    exemplo listas

---
+++ exemplo_vetores/src/main/java/school/sptech/ExemploListas.java
@@ -0,0 +1,41 @@
+package school.sptech;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploListas {
+
+    public static void main(String[] args) {
+        List<Integer> lista;
+
+        lista = new ArrayList<>();
+        lista.add(10);
+        lista.add(20);
+        lista.add(30);
+        lista.add(40);
+        lista.add(50);
+        lista.add(1, 70);
+        lista.set(2, 99);
+        Integer valor = 70;
+        lista.remove(valor);
+
+        System.out.println(lista);
+
+//      pegar um valor pelo index
+        System.out.println(lista.get(1));
+
+        List<String> frutas = new ArrayList<>(List.of("Fruta Pão", "Banana"));
+        frutas.add("Laranja");
+        frutas.add("Pera");
+        frutas.add("Goiaba");
+        frutas.add("Maçã");
+        frutas.addAll(List.of("Tomate", "Melão"));
+
+        System.out.println(frutas);
+
+        for (int i = 0; i < frutas.size(); i++) {
+
+            System.out.println(frutas.get(i));
+        }
+    }
+}
