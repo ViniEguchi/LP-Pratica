72031924ec8a15f104a1409665373832cad6c94c
ViniEguchi <vinicius.eguchi@sptech.school>
2025-03-26 20:15:28 -0300

    Exemplo sobre relacionamento entre classes

---
+++ exemplo_relacionamento/src/main/java/school/sptech/Main.java
@@ -0,0 +1,21 @@
+package school.sptech;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Usuario usuario1 = new Usuario("Murilo", "murilo.barbosa@sptech.school","10123456789");
+
+        Musica musica1 = new Musica("Resenha do arrocha", 892, "Eskini");
+        Musica musica2 = new Musica("Decidi ai", 230, "Matheus e Kaua");
+        Musica musica3 = new Musica("Você não vale nada", 515, "Calcinha preta");
+
+        Playlist playlist1 = new Playlist("ads b", usuario1);
+
+        playlist1.adicionarMusica(musica1);
+        playlist1.adicionarMusica(musica2);
+        playlist1.adicionarMusica(musica3);
+
+        System.out.println(playlist1);
+    }
+}
---
+++ exemplo_relacionamento/src/main/java/school/sptech/Playlist.java
@@ -0,0 +1,46 @@
+package school.sptech;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist {
+
+    private String nome;
+    private Usuario usuario;
+    private List<Musica> musicas;
+
+    public Playlist(String nome, Usuario usuario) {
+        this.nome = nome;
+        this.usuario = usuario;
+        this.musicas = new ArrayList<>();
+    }
+
+    public void adicionarMusica(Musica novaMusica) {
+        this.musicas.add(novaMusica);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Playlist{" +
+                "nome='" + nome + '\'' +
+                ", usuario=" + usuario +
+                ", musicas=" + musicas +
+                '}';
+    }
+}
